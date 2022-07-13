feb862907cd8c6aff3009d6125ec32ca8ef9085c
Flo Slv <florent3969@hotmail.fr>
2022-07-13 17:37:57 +0200

    Add nvim-cmp config

---
+++ nvim-lua/plugin/nvim-cmp.lua
@@ -0,0 +1,76 @@
+local cmp = require'cmp'
+local lspkind = require'lspkind'
+
+cmp.setup({
+--	snippet = {
+--		expand = function(args)
+--			require'luasnip'.lsp_expand(args.body)
+--		end
+--	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<C-i>'] = cmp.mapping.select_next_item(),
+		['<C-p'] = cmp.mapping.select_prev_item(),
+		['<CR>'] = cmp.mapping.confirm({ select = true })
+	}),
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered()
+	},
+	sources = cmp.config.sources({
+		{ name = 'nvim_lua' },
+		{ name = 'nvim_lsp', keyword_length=3 },
+		{ name = 'path' },
+--		{ name = 'luasnip' }
+	}, {
+		{ name = 'buffer', keyword_length=3 }
+	}),
+	formatting = {
+		format = lspkind.cmp_format {
+			with_text = true,
+			menu = {
+				buffer = '[BUF]',
+				nvim_lsp = '[LSP]',
+				nvim_lua = '[api]',
+				path = '[PATH]',
+--				luasnip = '[SNIP]'
+			}
+		}
+	},
+	enabled = function()
+		-- disable completion in comments
+		local context = require'cmp.config.context'
+		-- keep command mode completion enabled when cursor is in a comment
+		if vim.api.nvim_get_mode().mode == 'c' then
+			return true
+		else
+			return not context.in_treesitter_capture('comment')
+			and not context.in_syntax_group('Comment')
+		end
+	end
+})
+
+-- Setup lspconfig.
+local lspconfig = require'lspconfig'
+local capabilities = require'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local servers = { 'tsserver', 'graphql', 'cssls', 'html' }
+
+for _, server in ipairs(servers) do
+	lspconfig[server].setup {
+		capabilities = capabilities
+	}
+end
+
+--local sumneko_root_path = os.getenv('HOME') .. '/lua-language-server'
+--local sumneko_binary = sumneko_root_path .. '/bin/lua-language-server'
+
+--[[lspconfig.sumneko_lua.setup {
+	cmd = {
+		sumneko_binary, '-E', sumneko_root_path .. '/main.lua'
+	},
+	capabilities = capabilities
+}]]
