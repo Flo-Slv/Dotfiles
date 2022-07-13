e50e38ab73f71db6c7221479c920301d084bc825
Flo Slv <florent3969@hotmail.fr>
2022-07-13 21:04:30 +0200

    Packer code for first install

--- nvim/lua/packer-plugins.lua
+++ nvim/lua/packer-plugins.lua
@@ -1,3 +1,17 @@
+-- At first install
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+	packer_bootstrap = fn.system({
+		'git',
+		'clone',
+		'--depth',
+		'1',
+		'git@github.com/wbthomason/packer.nvim',
+		install_path
+	})
+end
+
 -- Plugins
 return require'packer'.startup({
 	function(use)
