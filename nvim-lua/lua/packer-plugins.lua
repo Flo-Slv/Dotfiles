47e74dc1d2436496e12aa1c45f3ac14ec37ae00a
Flo Slv <florent3969@hotmail.fr>
2022-07-13 12:21:09 +0200

    Init.lua: add telescope plugins

--- nvim-lua/lua/packer-plugins.lua
+++ nvim-lua/lua/packer-plugins.lua
@@ -1,7 +1,7 @@
 return require'packer'.startup({
 	function(use)
 		-- Packer can manage itself
-  		use 'wbthomason/packer.nvim'
+		use 'wbthomason/packer.nvim'
 
 		-- Colorscheme
 		use 'folke/tokyonight.nvim'
@@ -11,10 +11,17 @@ return require'packer'.startup({
 		use 'glepnir/dashboard-nvim'
 		use 'kyazdani42/nvim-tree.lua'
 		use 'gelguy/wilder.nvim'
+
+		-- Telescope
+		use {
+			'nvim-telescope/telescope.nvim', tag = '0.1.0',
+			requires = { {'nvim-lua/plenary.nvim'} }
+		}
+		use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
 	end,
 	config = {
     		git = {
-      			default_url_format = 'git@github.com:%s'
-    		}
-  	}
+				default_url_format = 'git@github.com:%s'
+			}
+	}
 })
