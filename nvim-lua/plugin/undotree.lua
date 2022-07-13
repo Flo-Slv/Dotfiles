0cb839185ba443e8ab48ffe44fefdf30bf535675
Flo Slv <florent3969@hotmail.fr>
2022-07-13 17:57:57 +0200

    Add undotree config

---
+++ nvim-lua/plugin/undotree.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_set_keymap('n', '<leader>u', ':UndotreeToggle<CR>', { noremap = true })
+
+vim.cmd [[
+	if has('persistent_undo')
+		let target_path = expand('~/Flo/Dotfiles/nvim-lua/lua/undodir')
+
+		" If the location does not exist,
+		" create the directory and any parent directories.
+		if !isdirectory(target_path)
+			call mkdir(target_path, 'p', 0700)
+		endif
+
+		let &undodir=target_path
+		set undofile
+	endif
+]]
