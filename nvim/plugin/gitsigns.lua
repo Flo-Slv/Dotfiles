8eb593b3331864f14bb6a95895f007a9a0640a35
Flo Slv <florent3969@hotmail.fr>
2022-07-14 21:31:58 +0200

    Gitsigns add TODO for parsing date

--- nvim/plugin/gitsigns.lua
+++ nvim/plugin/gitsigns.lua
@@ -47,7 +47,7 @@ require('gitsigns').setup {
 		delay = 1000,
 		ignore_whitespace = false,
 	},
-	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+	current_line_blame_formatter = '<author>, <author_time:%d-%m-%Y> - <summary>', -- TODO: find how to set date
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
