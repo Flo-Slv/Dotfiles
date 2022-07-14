056fe72e4cf842d1c023219b69249a4e9729f565
Flo Slv <florent3969@hotmail.fr>
2022-07-14 16:28:26 +0200

    Add luasnip plugins

--- nvim/lua/packer-plugins.lua
+++ nvim/lua/packer-plugins.lua
@@ -92,8 +92,10 @@ return require'packer'.startup({
 		use 'hrsh7th/cmp-buffer'
 		use 'hrsh7th/cmp-path'
 		use 'hrsh7th/cmp-cmdline'
-		use 'hrsh7th/cmp-nvim-lsp'
+		use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
 		use 'onsails/lspkind.nvim'
+		use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+		use 'L3MON4D3/LuaSnip' -- Snippets plugin
 
 		-- Databases
 		use 'tpope/vim-dadbod'
