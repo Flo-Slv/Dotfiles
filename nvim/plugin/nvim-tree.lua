312f02fa293e3b71ee68712e2780bcbac583d267
Flo Slv <florent3969@hotmail.fr>
2022-07-15 22:19:01 +0200

    NvimTree width option set to 30

--- nvim/plugin/nvim-tree.lua
+++ nvim/plugin/nvim-tree.lua
@@ -2,11 +2,7 @@ require'nvim-tree'.setup({
 	sort_by = 'case_sensitive',
 	view = {
 		adaptive_size = false,
-		mappings = {
-			list = {
-				{ key = 'u', action = 'dir_up' }
-			}
-		}
+		width = 30
 	},
 	renderer = {
 		group_empty = true
@@ -21,9 +17,17 @@ local options = { noremap = true }
 vim.api.nvim_set_keymap('n', '<C-a>', ':NvimTreeToggle<CR>', options)
 vim.api.nvim_set_keymap('n', '<C-f>', ':NvimTreeFindFile<CR>', options)
 
--- Create autocmd to quit NvimTree when last windows close.
+-- Autocmd to quit NvimTree when last windows close.
 -- There is an issue with auto_close options...
 vim.api.nvim_create_autocmd('BufEnter', {
 	command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
 	nested = true
 })
+
+-- Autocmd to disable Lightline statusline when open NvimTree.
+-- But it disable all statusline... Not what I want.
+-- vim.api.nvim_create_autocmd('BufWinEnter', {
+-- 	callback = function()
+-- 		vim.api.nvim_set_option_value('statusline', '%#Normal#', { scope = 'local' })
+-- 	end
+-- })
