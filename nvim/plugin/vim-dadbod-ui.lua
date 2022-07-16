b3ddc5327ca6628db097e0305c0ea219f27b53de
Flo Slv <florent3969@hotmail.fr>
2022-07-16 03:16:20 +0200

    Add config for vim-dadbod-ui

--- nvim/plugin/vim-dadbod-ui.lua
+++ nvim/plugin/vim-dadbod-ui.lua
@@ -2,3 +2,15 @@
 -- Connect to mongoDB: :DB mongodb+srv://<user>:<password>@flo.j1cmj.mongodb.net/<dbName>?retryWrites=true&w=majority
 
 vim.api.nvim_set_keymap('n', '<leader>mo', ':DBUIToggle<CR>', { noremap = true })
+
+vim.cmd [[
+	let g:db_ui_table_helpers = {
+	\	'mongodb+srv': {
+	\		'List': '{table}.find()'
+	\	}
+	\}
+]]
+
+vim.g.db_ui_auto_execute_table_helpers = 1
+vim.g.db_ui_save_location = '~/Flo/Dotfiles/nvim/lua/db'
+vim.g.db_ui_win_position = 'right'
