ecc384d576af7666ffd14f916860094166e08dbc
Flo Slv <florent3969@hotmail.fr>
2022-07-18 12:06:10 +0200

    Add lualine config

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -1,1 +1,22 @@
-require'lualine'.setup()
+require'lualine'.setup({
+	options = {
+		theme = 'nord', -- nightfly, nord, grubbox_dark, material, tokyionight
+	},
+	sections = {
+		lualine_a =  { 'mode' },
+		lualine_b = { 'branch' },
+		lualine_c = { 'filename' },
+		lualine_x = { '' },
+		lualine_y = { 'filetype' },
+		lualine_z = { 'progress' }
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {'filename'},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {}
+	},
+	extensions = { 'nvim-tree', 'fugitive' }
+})
