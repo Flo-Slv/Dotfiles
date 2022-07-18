5bb69332303b0b69894a73663e1516057cc70a06
Flo Slv <florent3969@hotmail.fr>
2022-07-18 12:46:05 +0200

    Add vimade basic config but not working

--- nvim/plugin/vimade.lua
+++ nvim/plugin/vimade.lua
@@ -1,1 +1,29 @@
-vim.g.vimade = {}
+-- require'vimade'
+vim.g.vimade = {
+	normalid = '',
+	normalncid = '',
+	basefg = '',
+	basebg = '',
+	fadelevel = 0.4,
+	colbufsize = 15,
+	rowbufsize = 15,
+	checkinterval = 100,
+	usecursorhold = 0,
+	detecttermcolors = 0,
+	enablescroll = 1,
+	enablesigns = 1,
+	signsid = 13100,
+	signsretentionperiod = 4000,
+	fademinimap = 1,
+	fadepriority = 10,
+	groupdiff = 1,
+	groupscrollbind = 0,
+	enablefocusfading = 0,
+	enablebasegroups = 1,
+	enabletreesitter = 0,
+	basegroups = { 'Folded', 'Search', 'SignColumn', 'LineNr', 'CursorLine', 'CursorLineNr', 'DiffAdd', 'DiffChange', 'DiffDelete', 'DiffText', 'FoldColumn', 'Whitespace' }
+}
+
+vim.g.enable = true
+vim.g.vimade.fadelevel = 0.7
+vim.g.vimade.enablesigns = true
