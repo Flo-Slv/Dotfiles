2ccc0fab6624d5a76a5620c7581a3c2a79078f61
Flo Slv <florent3969@hotmail.fr>
2022-07-20 21:42:37 +0200

    feat(changelog.config.js): add config file for git-cz

---
+++ git/changelog.config.js
@@ -0,0 +1,57 @@
+// For git-cz
+module.exports = {
+	disableEmoji: false,
+	format: '{type}{scope}: {emoji}{subject}',
+	list: ['test', 'feat', 'fix', 'docs', 'refactor', 'style', 'ci'],
+	maxMessageLength: 64,
+	minMessageLength: 3,
+	questions: ['type', 'scope', 'subject', 'body', 'breaking', 'issues', 'lerna'],
+	scopes: [],
+	enableWritingScopes: true, // Feature not yet push
+	types: {
+		ci: {
+			description: 'CI related changes',
+			emoji: '🎡',
+			value: 'ci'
+		},
+		docs: {
+			description: 'Documentation only changes',
+			emoji: '✏️',
+			value: 'docs'
+		},
+		feat: {
+			description: 'A new feature',
+			emoji: '🌞',
+			value: 'feat'
+		},
+		fix: {
+			description: 'A bug fix',
+			emoji: '🐛',
+			value: 'fix'
+		},
+		refactor: {
+			description: 'A code change that neither fixes a bug or adds a feature',
+			emoji: '💡',
+			value: 'refactor'
+		},
+		style: {
+			description: 'Markup, white-space, formatting, missing semi-colons...',
+			emoji: '💄',
+			value: 'style'
+		},
+		test: {
+			description: 'Adding missing tests',
+			emoji: '💍',
+			value: 'test'
+		},
+		messages: {
+			type: 'Select the type of change that you\'re committing:',
+			customScope: 'Select the scope this component affects:',
+			subject: 'Write a short, imperative mood description of the change:\n',
+			body: 'Provide a longer description of the change:\n ',
+			breaking: 'List any breaking changes:\n',
+			footer: 'Issues this commit closes, e.g #123:',
+			confirmCommit: 'The packages that this commit has affected\n'
+		}
+	}
+};
