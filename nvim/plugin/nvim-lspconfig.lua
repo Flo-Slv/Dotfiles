798d62e686318495e6743c2715b34f2a4f00a8a2
Flo Slv <florent3969@hotmail.fr>
2022-07-20 02:42:00 +0200

    lsp config: add custom function to change diagnostic border color to adap from Diagnostic event

--- nvim/plugin/nvim-lspconfig.lua
+++ nvim/plugin/nvim-lspconfig.lua
@@ -59,11 +59,19 @@ lsp.handlers['textDocument/signatureHelp'] = lsp.with(vim.lsp.handlers.hover, {
 })
 
 vim.diagnostic.config({
-	virtual_text = true,
+	virtual_text = false,
 	signs = true,
 	underline = true,
 	update_in_insert = true,
-	severity_sort = true
+	severity_sort = true,
+	float = {
+		focusable = false,
+		-- style = 'minimal',
+		-- border = 'rounded',
+		source = 'always',
+		header = '',
+		prefix = ''
+	}
 })
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
@@ -71,3 +79,51 @@ for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+
+-- Override default vim.diagnostic.open_float() function
+-- to change border and background color to adapt colors from Diagnostic event.
+vim.diagnostic.open_float = (function(orig)
+	return function(bufnr, opts)
+		local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+		local opts = opts or {}
+
+		-- A more robust solution would check the "scope" value in `opts` to
+		-- determine where to get diagnostics from, but if you're only using
+		-- this for your own purposes you can make it as simple as you like
+		local diagnostics = vim.diagnostic.get(opts.bufnr or 0, { lnum = lnum })
+		local max_severity = vim.diagnostic.severity.HINT
+
+		for _, d in ipairs(diagnostics) do
+			-- Equality is "less than" based on how the severities are encoded
+			if d.severity < max_severity then
+				max_severity = d.severity
+			end
+		end
+
+		local border_color = ({
+			[vim.diagnostic.severity.HINT] = "DiagnosticHint",
+			[vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+			[vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+			[vim.diagnostic.severity.ERROR] = "DiagnosticError"
+		})[max_severity]
+
+		opts.border = {
+			{ '╭', border_color },
+			{ '─', border_color },
+			{ '╮', border_color },
+			{ '│', border_color },
+			{ '╯', border_color },
+			{ '─', border_color },
+			{ '╰', border_color },
+			{ '│', border_color }
+		}
+
+		-- Change background color
+		vim.cmd [[ highlight DiagnosticBGColor guibg=#24283b]]
+		vim.api.nvim_win_set_option(0, 'winhl', 'Normal:DiagnosticBGColor')
+
+		-- Return new params
+		orig(bufnr, opts)
+	end
+end)(vim.diagnostic.open_float)
