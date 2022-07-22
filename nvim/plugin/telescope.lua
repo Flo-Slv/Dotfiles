9f85e1d90daa55617582e99ec20ce0158d7ae8da
Flo Slv <florent3969@hotmail.fr>
2022-07-22 14:24:51 +0200

    🟦 feat: Change telescope border close to white color

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -25,10 +25,15 @@ require'neoclip'.setup()
 
 
 -- UI
-vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg='#3B4252' })
-vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg='#3B4252' })
-vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { fg='#3B4252' })
-vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { fg='#3B4252' })
+vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg='#CCCCCC' })
+vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg='#CCCCCC' })
+vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { fg='#CCCCCC' })
+vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { fg='#CCCCCC' })
+
+-- vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg='#3B4252' })
+-- vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg='#3B4252' })
+-- vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { fg='#3B4252' })
+-- vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { fg='#3B4252' })
 
 vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { bg='#24283b' })
 vim.api.nvim_set_hl(0, 'TelescopeResultsNormal', { bg='#24283b' })
