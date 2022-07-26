2b029849789d86b1dcfd77b017d8e1eb589ba5b9
Flo Slv <florent3969@hotmail.fr>
2022-07-26 17:10:28 +0200

    🟥 fix: wrong path for undotree

--- nvim/plugin/undotree.lua
+++ nvim/plugin/undotree.lua
@@ -1,6 +1,6 @@
 vim.cmd [[
 	if has('persistent_undo')
-		let target_path = expand('~/Flo/Dotfiles/nvim-lua/lua/undodir')
+		let target_path = expand('~/Flo/Dotfiles/nvim/lua/undodir')
 
 		" If the location does not exist,
 		" create the directory and any parent directories.
